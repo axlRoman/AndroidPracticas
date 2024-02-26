95fb0d4c9ce7599da6beeb226e49630495fc80b3
Francisco AxelRoman Cardoza <franaxel@live.com.mx>
2024-02-26 09:05:35 -0600

    clase 26 de febrero 2024

--- U2CuadrosMensajeApp/app/src/main/java/mx/itl/nc19130971/u2cuadrosmensajeapp/MainActivity.java
+++ U2CuadrosMensajeApp/app/src/main/java/mx/itl/nc19130971/u2cuadrosmensajeapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -72,16 +74,83 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
     //----------------------------------------------------------------------------------------------
+    CharSequence colores [] = {"Verde", "Blanco", "Rojo", "Morado"};
     public void btnMensajeListaSimpleClick (View v) {
-
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Seleccione: ")
+                .setItems(colores, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Se selecciono un elemento de la lista
+                        mostrarToast("Seleccione colores: " + colores[which]);
+                    }
+                })
+                .create()
+                .show();
     }
     //----------------------------------------------------------------------------------------------
     public void btnMensajeOpcionesSelUnicaClick (View v) {
-
+        int colorDefinitivo = 0;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Seleccione un color: ")
+                .setSingleChoiceItems(colores, colorDefinitivo, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mostrarToast("Color seleccionado: " + colores[which]);
+                        //colorSeleccionado = which;
+                    }
+                })
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mostrarToast("Color definitivo: " + colores[which]);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Cerrar el cuadro de mensaje explicitamente
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(false) // anulamos que el cuadro se cierre al hacer tap fuera de el
+                .create()
+                .show();
     }
     //----------------------------------------------------------------------------------------------
+    //Cuadro de mensaje con lista de opciones con casillas de verificacion (checkboxes)
+    boolean coloresDefault [] = {true, false, false, true};
+    ArrayList<CharSequence> coloresFavoritos = new ArrayList<>();
     public void btnMensajeOpcionesSelMultipleClick (View v) {
-
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Seleccione sus colores favoritos:")
+                .setMultiChoiceItems(colores, coloresDefault, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        if(isChecked) {
+                            //Se marco un color favorito, agregarlo a los colores favoritos
+                            coloresFavoritos.add(colores[which]);
+                        } else {
+                            //Se desmarco un color favorito, eliminarlo de colores favoritos
+                            coloresFavoritos.remove(colores[which]);
+                        }
+                    }
+                })
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mostrarToast("Sus colores favoritos son: " + coloresFavoritos);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(false)
+                .create()
+                .show();
     }
     //----------------------------------------------------------------------------------------------
     public void btnMensajeLayoutIncrustadoClick (View v) {
